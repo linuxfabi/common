21610570541be1a4077e1003428ae4422427fe29
Fabienne Fedier <fabienne.fedier@gmail.com>
2017-06-24 23:17:05 +0200

    Initialize git repository for common classes (change from SVN)

--- Mail.php
+++ Mail.php
@@ -14,6 +14,7 @@ class Mail {
     
     function __construct() {}
     
+    // setter
     public function addTo($to)           {$this->toList[]   = $to;}
     public function addCC($cc)           {$this->ccList[]   = $cc;}
     public function addBCC($bcc)         {$this->bccList[]  = $bcc;}
@@ -22,17 +23,22 @@ class Mail {
     public function setHeader($header)   {$this->mailHeader = $header;}
     public function setTextMsg($textMsg) {$this->textMsg    = $textMsg;}
     public function setHTMLMsg($htmlMsg) {$this->htmlMsg    = $htmlMsg;}
+    
+    // load textbody from file
     public function loadTextFile($path) {
         if (is_file($path)) {
             $this->setTextMsg(file_get_contents($path));
         }
     }
+    
+    // load html body from file
     public function loadHTMLFile($path) {
         if (is_file($path)) {
             $this->setHTMLMsg(file_get_contents($path));
         }
     }
     
+    // build the mail header
     private function _buildHeader() {
         $header  = 'MIME-Version: 1.0'."\r\n";
         $header .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
@@ -58,11 +64,29 @@ class Mail {
         $this->setHeader($header);
     }
     
+    // send function
     public function send() {
         if (empty($this->mailHeader)) {
             $this->_buildHeader();
         }
-        mail($this->to[0], $this->subject, $this->textMsg, $this->mailHeader);
+        
+        if (!empty($this->toList)) {
+          foreach($this->toList as $curTo) {
+            mail($curTo, $this->subject, $this->textMsg, $this->mailHeader);
+          }
+        }
+        
+        if (!empty($this->ccList)) {
+          foreach($this->ccList as $curCC) {
+            mail($curCC, $this->subject, $this->textMsg, $this->mailHeader);
+          }
+        }
+        
+        if (!empty($this->bccList)) {
+          foreach($this->bccList as $curBCC) {
+            mail($curBCC, $this->subject, $this->textMsg, $this->mailHeader);
+          }
+        }
     }
 }
 
